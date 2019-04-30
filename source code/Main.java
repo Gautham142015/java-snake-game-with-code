e4affc050b106d7258f8ec4bbc13593e25542f2e
gautham Balaji <31135334+kakakakaotha@users.noreply.github.com>
2019-04-30 18:43:28 +0530

    Add files via upload

---
+++ source code/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Window f1= new Window();
+		
+		f1.setTitle("Snake");
+		f1.setSize(300,300);
+		f1.setVisible(true);
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+
+	}
+}
